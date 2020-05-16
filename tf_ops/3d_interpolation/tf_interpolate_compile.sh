32397a96f3a9b943e34f7893d3211f8b954bd2c6
irfan798 <irfanbilaloglu@gmail.com>
2020-05-16 19:35:52 +0300

    tf_ops compile scripts for Tensorflow 1.15.0 - Ubuntu 18.04 - CUDA 10.2

--- tf_ops/3d_interpolation/tf_interpolate_compile.sh
+++ tf_ops/3d_interpolation/tf_interpolate_compile.sh
@@ -1,5 +1,9 @@
-TF_INC=/home/vcortex/anaconda3/envs/tensorflow/lib/python3.7/site-packages/tensorflow_core/include
-TF_LIB=/home/vcortex/anaconda3/envs/tensorflow/lib/python3.7/site-packages/tensorflow_core
+#TF_INC=/home/vcortex/anaconda3/envs/tensorflow36/lib/python3.6/site-packages/tensorflow_core/include
+#TF_LIB=/home/vcortex/anaconda3/envs/tensorflow36/lib/python3.6/site-packages/tensorflow_core
+#TF_LIB=/home/vcortex/anaconda3/envs/tensorflow36/lib/python3.6/site-packages/tensorflow_core/
+
+TF_INC=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_include())')
+TF_LIB=$(python -c 'import tensorflow as tf; print(tf.sysconfig.get_lib())')
 
 # TF1.2
 #g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -lcudart -L /usr/local/cuda-8.0/lib64/ -O2 -D_GLIBCXX_USE_CXX11_ABI=0
@@ -7,4 +11,4 @@ TF_LIB=/home/vcortex/anaconda3/envs/tensorflow/lib/python3.7/site-packages/tenso
 # TF1.4
 #g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I /usr/local/lib/python2.7/dist-packages/tensorflow/include -I /usr/local/cuda-8.0/include -I /usr/local/lib/python2.7/dist-packages/tensorflow/include/external/nsync/public -lcudart -L /usr/local/cuda-8.0/lib64/ -L/usr/local/lib/python2.7/dist-packages/tensorflow -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
 
-g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /usr/include -I $TF_INC/external/nsync/public -lcudart -L /usr/lib/cuda/lib64/ -L$TF_LIB -ltensorflow_framework -O2 -D_GLIBCXX_USE_CXX11_ABI=0
+g++ -std=c++11 tf_interpolate.cpp -o tf_interpolate_so.so -shared -fPIC -I $TF_INC -I /usr/local/cuda/include -I $TF_INC/external/nsync/public -lcudart -L /usr/local/cuda/lib64/ -L$TF_LIB -ltensorflow_framework #-O2 -D_GLIBCXX_USE_CXX11_ABI=1
